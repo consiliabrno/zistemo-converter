89d0c10d3e9d52c495e87c9adffb24843d0945a5
dvlastnik <xvlastni@mendelu.cz>
2023-08-09 15:21:02 +0200

    updated

--- app.py
+++ app.py
@@ -17,15 +17,17 @@ def is_allowed_file(filename: str):
 
 def delete_output():
     folder = "static/output"
-    for file_name in os.listdir(folder):
-        file_path = os.path.join(folder, file_name)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+    if os.path.exists(folder):
+        for file_name in os.listdir(folder):
+            if (file_name != ".gitkeep"):
+                file_path = os.path.join(folder, file_name)
+                try:
+                    if os.path.isfile(file_path) or os.path.islink(file_path):
+                        os.unlink(file_path)
+                    elif os.path.isdir(file_path):
+                        shutil.rmtree(file_path)
+                except Exception as e:
+                    print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 @app.route("/", methods=["GET", "POST"])
 def index():
